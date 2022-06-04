15f8f27e19df394fee12f6462ba8acc867a3a510
Fero.Feroov <feroovt@gmail.com>
2022-06-04 17:42:48 +0200

    Game loop set

--- src/com/feroov/inputs/KeyboardInputs.java
+++ src/com/feroov/inputs/KeyboardInputs.java
@@ -1,10 +1,18 @@
 package com.feroov.inputs;
 
+import com.feroov.main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener
 {
+    private GamePanel gamePanel;
+
+    public KeyboardInputs(GamePanel gamePanel)
+    {
+        this.gamePanel = gamePanel;
+    }
 
     @Override
     public void keyTyped(KeyEvent e)
@@ -15,13 +23,7 @@ public class KeyboardInputs implements KeyListener
     @Override
     public void keyPressed(KeyEvent e)
     {
-        switch (e.getKeyCode())
-        {
-            case KeyEvent.VK_W -> System.out.println("W");
-            case KeyEvent.VK_A -> System.out.println("A");
-            case KeyEvent.VK_S -> System.out.println("S");
-            case KeyEvent.VK_D -> System.out.println("D");
-        }
+
     }
 
     @Override
--- src/com/feroov/main/GamePanel.java
+++ src/com/feroov/main/GamePanel.java
@@ -1,16 +1,36 @@
 package com.feroov.main;
 
 import com.feroov.inputs.KeyboardInputs;
+import com.feroov.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel
 {
+    private MouseInputs mouseInputs;
+    private int frames = 0;
+    private long lastCheck = 0;
+
     public GamePanel()
     {
-        addKeyListener(new KeyboardInputs());
+        mouseInputs = new MouseInputs(this);
+
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
-    public void paintComponent(Graphics g) { super.paintComponent(g); }
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        frames++;
+        if(System.currentTimeMillis() - lastCheck >= 1000)
+        {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS:" + frames);
+            frames = 0;
+        }
+    }
 }
+
